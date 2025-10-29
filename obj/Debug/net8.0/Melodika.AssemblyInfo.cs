87e8a8ad9989e6501343ad8a7db3101b72803cc5
IVAN MOSCOVICH LEVI <48802641@ort>
2025-10-29 13:54:26 -0300

    foto

--- obj/Debug/net8.0/Melodika.AssemblyInfo.cs
+++ obj/Debug/net8.0/Melodika.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1accaa73dd8b1b2a6b9f849179aa5f107b095854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28583475220bf88b4fc5d435b4cf88d49981e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
