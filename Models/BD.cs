aae8831d8d3eec7b98a892c229481e4e3c96ad51
FEDERICO IAIR DRUCKER <49092806@ort>
2025-10-30 11:44:14 -0300

    controller y view base de canciones destacadas

--- Models/BD.cs
+++ Models/BD.cs
@@ -2,8 +2,20 @@ namespace melodika.Models;
 using System;
 using Microsoft.Data.SqlClient;
 using Dapper;
+using System.Collections.Generic;
 public static class BD
 {
     private static string _connectionString = @"Server=localhost; DataBase = Melodika_DB; Trusted_Connection = true; TrustServerCertificate = true";
+
+     static public List<Cancion> seleccionarCancion()
+    {
+        List<Cancion> canciones = new List<Cancion>();
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "EXEC seleccionarCancion;";
+            canciones = connection.Query<Cancion>(query).ToList();
+        }
+        return canciones;
+    }
 
 }
