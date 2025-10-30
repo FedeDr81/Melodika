98b145e80ff3349b8c44999300b6c4d6484eed17
JUAN MANUEL SABATHIE LOPEZ <49378703@ort.edu.ar>
2025-10-30 08:01:05 -0300

    30/10

--- obj/Debug/net8.0/Melodika.AssemblyInfo.cs
+++ obj/Debug/net8.0/Melodika.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e8a8ad9989e6501343ad8a7db3101b72803cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65ce6307f6876df3d2344461602a2de5f7a8816")]
 [assembly: System.Reflection.AssemblyProductAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
