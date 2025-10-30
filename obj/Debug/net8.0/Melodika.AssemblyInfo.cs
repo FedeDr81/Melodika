81c42d06114604231e7dce78184d7cdc4491ec36
JUAN MANUEL SABATHIE LOPEZ <49378703@ort.edu.ar>
2025-10-30 14:56:46 -0300

    landing casi terminada

--- obj/Debug/net8.0/Melodika.AssemblyInfo.cs
+++ obj/Debug/net8.0/Melodika.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4299dcd3c75717e61233db9c02e568d4738229a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b592ff5eae5e0c60e939bc06a00386ae65cc8ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Melodika")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
